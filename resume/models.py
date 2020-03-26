0484424348a657f9ae405e0004aa5a8cc97e3407
DmitryKos <dmitrykospts@gmail.com>
2020-03-26 22:23:15 +0300

    some fixes

--- resume/models.py
+++ resume/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Resume(models.Model):
-    """Поля приложения "Resume"""
+    """РџРѕР»СЏ РїСЂРёР»РѕР¶РµРЅРёСЏ "Resume"""
     title = models.CharField(max_length=50)
     text = models.CharField(max_length=1000)
-    date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now=True, blank=True)
 
     def __str__(self):
         return self.title
