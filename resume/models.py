abce755adce5eed3cf45789fd5544f7d219edce8
Dmitry <dmitry@dmitry.ru>
2020-03-26 16:08:07 +0300

    added resume app

---
+++ resume/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Resume(models.Model):
+    """Поля приложения "Resume"""
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=1000)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
